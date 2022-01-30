11a538d25b703dcac78008001d293e93cf3f0ac9
JasonWandrag <jasonwandrag@hotmail.com>
2022-01-30 23:28:51 +0200

    update styles

---
+++ dist/precache-manifest.322746ebd3d18c2e034b9ac581585f06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5bb45dc8f0935d098e",
+    "url": "/css/about.ebf701be.css"
+  },
+  {
+    "revision": "a235e757ec4e9c1beaaf",
+    "url": "/css/app.e99ba69e.css"
+  },
+  {
+    "revision": "6d5326efa357ac15244ec52965b1b83c",
+    "url": "/img/TeleMedicine.6d5326ef.png"
+  },
+  {
+    "revision": "74e1d8156937712aaf9745d9ca2d5511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e5bb45dc8f0935d098e",
+    "url": "/js/about.8ff839ca.js"
+  },
+  {
+    "revision": "a235e757ec4e9c1beaaf",
+    "url": "/js/app.acafe978.js"
+  },
+  {
+    "revision": "c09d2cf262a25867c401",
+    "url": "/js/chunk-vendors.d7ab5141.js"
+  },
+  {
+    "revision": "e6d3860f816ecd3d2029268f61e73625",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
